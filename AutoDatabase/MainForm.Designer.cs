f468707bbfbb8a3922c823d3669b2f7b03b4bdf1
Sarunas <visarunas@gmail.com>
2016-11-29 22:39:30 +0200

    Add project files.

---
+++ AutoDatabase/MainForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WindowsFormsApplication3
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.RegistrateClientTab = new System.Windows.Forms.TabPage();
+			this.BuyPartsTab = new System.Windows.Forms.TabPage();
+			this.JobsTab = new System.Windows.Forms.TabPage();
+			this.EmployeesTab = new System.Windows.Forms.TabPage();
+			this.PartsForClientTab = new System.Windows.Forms.TabPage();
+			this.DocumentsTab = new System.Windows.Forms.TabPage();
+			this.SearchTab = new System.Windows.Forms.TabPage();
+			this.DataEntryTab = new System.Windows.Forms.TabPage();
+			this.DataEditTab = new System.Windows.Forms.TabPage();
+			this.SystemLogTab = new System.Windows.Forms.TabPage();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.tabControl1.SuspendLayout();
+			this.SystemLogTab.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.RegistrateClientTab);
+			this.tabControl1.Controls.Add(this.BuyPartsTab);
+			this.tabControl1.Controls.Add(this.JobsTab);
+			this.tabControl1.Controls.Add(this.EmployeesTab);
+			this.tabControl1.Controls.Add(this.PartsForClientTab);
+			this.tabControl1.Controls.Add(this.DocumentsTab);
+			this.tabControl1.Controls.Add(this.SearchTab);
+			this.tabControl1.Controls.Add(this.DataEntryTab);
+			this.tabControl1.Controls.Add(this.DataEditTab);
+			this.tabControl1.Controls.Add(this.SystemLogTab);
+			this.tabControl1.Location = new System.Drawing.Point(-1, 1);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(857, 561);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// RegistrateClientTab
+			// 
+			this.RegistrateClientTab.Location = new System.Drawing.Point(4, 22);
+			this.RegistrateClientTab.Name = "RegistrateClientTab";
+			this.RegistrateClientTab.Padding = new System.Windows.Forms.Padding(3);
+			this.RegistrateClientTab.Size = new System.Drawing.Size(849, 535);
+			this.RegistrateClientTab.TabIndex = 0;
+			this.RegistrateClientTab.Text = "Registruoti klientą";
+			this.RegistrateClientTab.UseVisualStyleBackColor = true;
+			// 
+			// BuyPartsTab
+			// 
+			this.BuyPartsTab.Location = new System.Drawing.Point(4, 22);
+			this.BuyPartsTab.Name = "BuyPartsTab";
+			this.BuyPartsTab.Padding = new System.Windows.Forms.Padding(3);
+			this.BuyPartsTab.Size = new System.Drawing.Size(849, 535);
+			this.BuyPartsTab.TabIndex = 1;
+			this.BuyPartsTab.Text = "Pirkti detales";
+			this.BuyPartsTab.UseVisualStyleBackColor = true;
+			// 
+			// JobsTab
+			// 
+			this.JobsTab.Location = new System.Drawing.Point(4, 22);
+			this.JobsTab.Name = "JobsTab";
+			this.JobsTab.Padding = new System.Windows.Forms.Padding(3);
+			this.JobsTab.Size = new System.Drawing.Size(849, 535);
+			this.JobsTab.TabIndex = 9;
+			this.JobsTab.Text = "Darbai";
+			this.JobsTab.UseVisualStyleBackColor = true;
+			// 
+			// EmployeesTab
+			// 
+			this.EmployeesTab.Location = new System.Drawing.Point(4, 22);
+			this.EmployeesTab.Name = "EmployeesTab";
+			this.EmployeesTab.Padding = new System.Windows.Forms.Padding(3);
+			this.EmployeesTab.Size = new System.Drawing.Size(849, 535);
+			this.EmployeesTab.TabIndex = 4;
+			this.EmployeesTab.Text = "Darbuotojai";
+			this.EmployeesTab.UseVisualStyleBackColor = true;
+			// 
+			// PartsForClientTab
+			// 
+			this.PartsForClientTab.Location = new System.Drawing.Point(4, 22);
+			this.PartsForClientTab.Name = "PartsForClientTab";
+			this.PartsForClientTab.Padding = new System.Windows.Forms.Padding(3);
+			this.PartsForClientTab.Size = new System.Drawing.Size(849, 535);
+			this.PartsForClientTab.TabIndex = 5;
+			this.PartsForClientTab.Text = "Detalės klientui";
+			this.PartsForClientTab.UseVisualStyleBackColor = true;
+			// 
+			// DocumentsTab
+			// 
+			this.DocumentsTab.Location = new System.Drawing.Point(4, 22);
+			this.DocumentsTab.Name = "DocumentsTab";
+			this.DocumentsTab.Padding = new System.Windows.Forms.Padding(3);
+			this.DocumentsTab.Size = new System.Drawing.Size(849, 535);
+			this.DocumentsTab.TabIndex = 6;
+			this.DocumentsTab.Text = "Dokumentai";
+			this.DocumentsTab.UseVisualStyleBackColor = true;
+			// 
+			// SearchTab
+			// 
+			this.SearchTab.Location = new System.Drawing.Point(4, 22);
+			this.SearchTab.Name = "SearchTab";
+			this.SearchTab.Padding = new System.Windows.Forms.Padding(3);
+			this.SearchTab.Size = new System.Drawing.Size(849, 535);
+			this.SearchTab.TabIndex = 7;
+			this.SearchTab.Text = "Paieška";
+			this.SearchTab.UseVisualStyleBackColor = true;
+			// 
+			// DataEntryTab
+			// 
+			this.DataEntryTab.Location = new System.Drawing.Point(4, 22);
+			this.DataEntryTab.Name = "DataEntryTab";
+			this.DataEntryTab.Padding = new System.Windows.Forms.Padding(3);
+			this.DataEntryTab.Size = new System.Drawing.Size(849, 535);
+			this.DataEntryTab.TabIndex = 8;
+			this.DataEntryTab.Text = "Duomenų įvedimas";
+			this.DataEntryTab.UseVisualStyleBackColor = true;
+			// 
+			// DataEditTab
+			// 
+			this.DataEditTab.Location = new System.Drawing.Point(4, 22);
+			this.DataEditTab.Name = "DataEditTab";
+			this.DataEditTab.Padding = new System.Windows.Forms.Padding(3);
+			this.DataEditTab.Size = new System.Drawing.Size(849, 535);
+			this.DataEditTab.TabIndex = 3;
+			this.DataEditTab.Text = "Redaguoti duomenis";
+			this.DataEditTab.UseVisualStyleBackColor = true;
+			// 
+			// SystemLogTab
+			// 
+			this.SystemLogTab.Controls.Add(this.richTextBox1);
+			this.SystemLogTab.Location = new System.Drawing.Point(4, 22);
+			this.SystemLogTab.Name = "SystemLogTab";
+			this.SystemLogTab.Padding = new System.Windows.Forms.Padding(3);
+			this.SystemLogTab.Size = new System.Drawing.Size(849, 535);
+			this.SystemLogTab.TabIndex = 10;
+			this.SystemLogTab.Text = "Sistemos žurnalas";
+			this.SystemLogTab.UseVisualStyleBackColor = true;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.richTextBox1.Location = new System.Drawing.Point(8, 40);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(818, 490);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "2016-11-09  16:03 Vartotojas Darbuotojas1 prisijungė į sistemą\n2016-11-09  16:04 " +
+    "Užregistruotas naujas klientas Vardenis Pavardenis, Valst. Nr. AAA000\n\n";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(856, 564);
+			this.Controls.Add(this.tabControl1);
+			this.Name = "MainForm";
+			this.Text = "Mechanikas";
+			this.tabControl1.ResumeLayout(false);
+			this.SystemLogTab.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage RegistrateClientTab;
+		private System.Windows.Forms.TabPage BuyPartsTab;
+		private System.Windows.Forms.TabPage DataEditTab;
+		private System.Windows.Forms.TabPage EmployeesTab;
+		private System.Windows.Forms.TabPage PartsForClientTab;
+		private System.Windows.Forms.TabPage DocumentsTab;
+		private System.Windows.Forms.TabPage SearchTab;
+		private System.Windows.Forms.TabPage DataEntryTab;
+		private System.Windows.Forms.TabPage JobsTab;
+		private System.Windows.Forms.TabPage SystemLogTab;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
